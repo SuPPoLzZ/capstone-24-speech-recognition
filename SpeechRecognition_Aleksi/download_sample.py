e7e7d2ba9f2842b52ea196f16e10c4a320c7de4b
tsubasa <tsubasa.kesennuma@gmail.com>
2025-02-25 18:05:34 +0900

    download program sample

---
+++ SpeechRecognition_Aleksi/download_sample.py
@@ -0,0 +1,27 @@
+import requests
+import os
+import zipfile
+import shutil
+
+def download_file(url, filename):
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        script_path = __file__  # スクリプト自身のファイルパス
+        script_directory = os.path.dirname(script_path)  # スクリプトのディレクトリパス
+        filepath = script_directory+"/"+filename
+        with open(filepath, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+        print(f"{filename} をダウンロードしました。")
+    else:
+        print(f"{url} からのダウンロードに失敗しました。ステータスコード: {response.status_code}")
+
+# 例
+url = "https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip"  # ダウンロードしたいファイルのURL
+filename = "file.zip"  # 保存するファイル名
+download_file(url, filename)
+
+script_path = __file__  # スクリプト自身のファイルパス
+script_directory = os.path.dirname(script_path)  # スクリプトのディレクトリパス
+shutil.unpack_archive(script_directory+'/file.zip', 'vosk')
+os.remove(script_directory+'/file.zip')
