1e6263f91665b73fd88e5da50a02bd2ea855dd47
Haaksi <126588878+Haaksi@users.noreply.github.com>
2025-04-04 11:29:54 +0300

    turned the VideoTEST into a function

--- SpeechRecognition_Aleksi/SimpleVideoTEST.py
+++ SpeechRecognition_Aleksi/SimpleVideoTEST.py
@@ -9,36 +9,39 @@ tello.streamon()
 # Delay for start
 time.sleep(2)
 
-# Define the video writer
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 out = cv2.VideoWriter("videos/tello_video.mp4", fourcc, 30.0, (960, 720))
 
-if not out.isOpened():
-    print("Error: VideoWriter failed to open!")
-else:
-    print("VideoWriter is working.")
 
-frame_count = 0
+def TakeDroneVideo():
+    if not out.isOpened():
+        print("Error: VideoWriter failed to open!")
+    else:
+        print("VideoWriter is working.")
 
-while True:
-    frame = tello.get_frame_read().frame
+    frame_count = 0
 
-    if frame is None:
-        print("Error: No frames recieved!")
-        continue
+    while True:
+        frame = tello.get_frame_read().frame
 
-    # frame = cv2.resize(frame, (640, 480))
-    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        if frame is None:
+            print("Error: No frames recieved!")
+            continue
 
-    out.write(frame)  # Save frame to file
-    frame_count += 1
-    cv2.imshow("Tello Video Stream", frame)
+        # frame = cv2.resize(frame, (640, 480))
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        out.write(frame)  # Save frame to file
+        frame_count += 1
+        cv2.imshow("Tello Video Stream", frame)
 
-print(f"✅ Video saved with {frame_count} frames")
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
+    print(f"✅ Video saved with {frame_count} frames")
+    out.release()  # Stop recording
+    cv2.destroyAllWindows()
+
+
+TakeDroneVideo()
 tello.streamoff()
-out.release()  # Stop recording
-cv2.destroyAllWindows()
